a018c9187419dfbbef820daf89f4d48c5ac56a21
Linda Österreicher <lindaoest@gmail.com>
2024-05-20 13:26:23 +0200

    side-menu component

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SiteMenuComponent } from './site-menu/site-menu.component';
+import { ChatComponent } from './chat/chat.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [SiteMenuComponent, ChatComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
