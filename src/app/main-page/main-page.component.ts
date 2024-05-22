2c0cda8f67775ff455d5c8f634313e9636885a3d
Linda Österreicher <lindaoest@gmail.com>
2024-05-22 16:59:49 +0200

    chat component + thred component

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SiteMenuComponent } from './site-menu/site-menu.component';
 import { ChatComponent } from './chat/chat.component';
+import { ThreadReplyComponent } from './thread-reply/thread-reply.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [SiteMenuComponent, ChatComponent],
+  imports: [SiteMenuComponent, ChatComponent, ThreadReplyComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
