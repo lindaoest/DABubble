d816586b594c7d3a3c9f07ca64397e55129d1596
lindaoest <131020218+lindaoest@users.noreply.github.com>
2024-06-07 14:01:48 +0200

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,12 +7,4 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp(
-    {"projectId":"da-bubble-88b90",
-      "appId":"1:380023840169:web:686b7738004d68493e38b8",
-      "storageBucket":"da-bubble-88b90.appspot.com",
-      "apiKey":"AIzaSyDiJdL9nmiEeGNm25HYVbdUrMkmCXbcOPY",
-      "authDomain":"da-bubble-88b90.firebaseapp.com",
-      "messagingSenderId":"380023840169"
-    }))), importProvidersFrom(provideFirestore(() => getFirestore()))]
-};
+  providers: [provideRouter(routes), provideAnimationsAsync(), importProvidersFrom(provideFirestore(() => getFirestore()))]};
