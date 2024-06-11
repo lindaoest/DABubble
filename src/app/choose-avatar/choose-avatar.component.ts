5335440384056536b5fa0c795e2bfa852591e15c
Linda Österreicher <lindaoest@gmail.com>
2024-06-11 18:30:16 +0200

    upload image on cloud storage

--- src/app/choose-avatar/choose-avatar.component.ts
+++ src/app/choose-avatar/choose-avatar.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
-import { getStorage, ref, getDownloadURL } from "firebase/storage";
+import { Component, inject } from '@angular/core';
+import { getStorage, ref, getDownloadURL, uploadBytes } from "firebase/storage";
 import { FormsModule } from '@angular/forms';
 import { FirestoreService } from '../shared/services/firestore/firestore.service';
 import { finalize } from 'rxjs/operators';
+import { Firestore } from '@angular/fire/firestore';
+import { GlobalVariablesService } from '../shared/services/global-variables/global-variables.service';
 
 @Component({
   selector: 'app-choose-avatar',
@@ -17,11 +19,62 @@ export class ChooseAvatarComponent {
   fileToUpload: File | null = null;
   file: File | null = null;
 
-  constructor(private channelFirestore: FirestoreService) { }
+
+
+  constructor(public firestore: Firestore, public globalVariables: GlobalVariablesService) { }
 
   addImage(item: any) {
     console.log(item)
     this.profilePicture = `./assets/img/avatar/avatar-${item}.svg`;
+  }
+
+  handleFileInput(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.file = file;
+
+      // getDownloadURL(starsRef)
+      //   .then((url) => {
+      //     console.log(url)
+      //   })
+      //   .catch((error) => {
+      //     // A full list of error codes is available at
+      //     // https://firebase.google.com/docs/storage/web/handle-errors
+      //     switch (error.code) {
+      //       case 'storage/object-not-found':
+      //         // File doesn't exist
+      //         break;
+      //       case 'storage/unauthorized':
+      //         // User doesn't have permission to access the object
+      //         break;
+      //       case 'storage/canceled':
+      //         // User canceled the upload
+      //         break;
+
+      //       // ...
+
+      //       case 'storage/unknown':
+      //         // Unknown error occurred, inspect the server response
+      //         break;
+      //     }
+      //   });
+    }
+  }
+
+  addMember() {
+    if (this.file) {
+      const storage = getStorage();
+      const starsRef = ref(storage, `images/${this.file.name}`);
+
+      uploadBytes(starsRef, this.file).then((snapshot) => {
+        console.log('Uploaded a blob or file!');
+        console.log(snapshot)
+        console.log(this.file)
+      });
+
+      this.globalVariables.newMember[0].avatar = this.file.name;
+      console.log(this.globalVariables.newMember)
+    }
   }
 
   // onSubmit() {
@@ -49,12 +102,4 @@ export class ChooseAvatarComponent {
   //     console.error('Error saving file URL in Firestore: ', error);
   //   });
   // }
-
-  handleFileInput(event: any) {
-    const file: File = event.target.files[0];
-    if (file) {
-      this.file = file;
-      console.log(this.file)
-    }
-  }
 }
