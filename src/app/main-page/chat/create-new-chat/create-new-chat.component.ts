6d74b84a530389569678f0a46929e449b0141927
Linda Österreicher <lindaoest@gmail.com>
2024-07-14 11:30:12 +0200

    add direct-message Component

--- src/app/main-page/chat/create-new-chat/create-new-chat.component.ts
+++ src/app/main-page/chat/create-new-chat/create-new-chat.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FirestoreService } from '../../../shared/services/firestore/firestore.service';
 
 @Component({
   selector: 'app-create-new-chat',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-new-chat.component.html',
   styleUrl: './create-new-chat.component.scss'
 })
 export class CreateNewChatComponent {
 
+  placeholder_value: string = "An: #channel, oder @jemand";
+  name: string = '';
+  message_to_direct_message: boolean = false;
+
+  constructor(public channelFirestore: FirestoreService) {}
+
+  observe_input() {
+    if(this.name.includes('@')) {
+      this.message_to_direct_message = true;
+    }
+  }
+
+  addMember(m:string) {
+    this.name = '@' + m;
+    this.message_to_direct_message = false;
+  }
+
+  sendMessage() {
+    
+  }
 }
