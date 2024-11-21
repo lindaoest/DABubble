e76ff95cf0b4eb1f6ecc2a44c7a92cb017eecd93
Linda Österreicher <lindaoest@gmail.com>
2024-11-21 18:18:28 +0100

    Add Locale_id for datePipe translations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,24 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { registerLocaleData } from '@angular/common';
+import localDe from '@angular/common/locales/de';
 
 import { privateConfig } from './app.config-private';
 
+registerLocaleData(localDe)
+
 export const appConfig: ApplicationConfig = {
-	providers: [provideRouter(routes), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp(
-	privateConfig
-	))), importProvidersFrom(provideFirestore(() => getFirestore()))]
-	}
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(
+      privateConfig
+    ))),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    {provide: LOCALE_ID, useValue: 'de-DE'}
+  ]
+}
