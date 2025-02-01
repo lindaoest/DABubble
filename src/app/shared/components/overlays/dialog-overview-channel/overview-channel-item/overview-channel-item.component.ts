d847c305e852ba3bf49aee225dd71dc64d7a3c47
Linda Österreicher <lindaoest@gmail.com>
2025-02-01 22:33:39 +0100

    Update channel routerLink & save new channelName

--- src/app/shared/components/overlays/dialog-overview-channel/overview-channel-item/overview-channel-item.component.ts
+++ src/app/shared/components/overlays/dialog-overview-channel/overview-channel-item/overview-channel-item.component.ts
@@ -5,6 +5,8 @@ import { Channel } from '../../../../../../models/channel.class';
 import { FormsModule } from '@angular/forms';
 import { FirestoreService } from '../../../../services/firestore/firestore.service';
 import { Router } from '@angular/router';
+import { Message } from '../../../../../../models/message.class';
+import { Thread } from '../../../../../../models/thread.class';
 
 @Component({
   selector: 'app-overview-channel-item',
@@ -49,7 +51,10 @@ export class OverviewChannelItemComponent {
   public saveChannel() {
     switch (this.key) {
       case 'name':
+        this.updateMessageFunction(this.text);
+        this.updateThreadFunction(this.text);
         this.data.name = this.text;
+        this.changeRouterLink();
         break;
       case 'description':
         this.data.description = this.text;
@@ -60,5 +65,60 @@ export class OverviewChannelItemComponent {
     this.firestoreService.updateChannel('channels', this.data);
     this.isEditable = false;
     this.globalVariables.activeChat = this.data.name;
+  }
+
+  public changeRouterLink() {
+    this.router.navigate(['/channels', this.data.name.toLowerCase()]);
+  }
+
+  public updateMessageFunction(name: string) {
+    const updateMessages = this.firestoreService.messages.filter(message => message.channel === this.data.name);
+
+    updateMessages.forEach(updateMessage => {
+      const message: Message = {
+        id: updateMessage.id,
+        channel: name,
+        text: updateMessage.text,
+        time: updateMessage.time,
+        sender: updateMessage.sender,
+        avatar: updateMessage.avatar,
+        creationDate: updateMessage.creationDate,
+        timeStamp: updateMessage.timeStamp
+      }
+      this.firestoreService.updateMessage('messages', message)
+    })
+  }
+
+  public updateThreadFunction(name: string) {
+    const updateThread = this.firestoreService.threads.filter(thread => thread.channel === this.data.name);
+
+    updateThread.forEach(updateThread => {
+      const thread: Thread = {
+        id: updateThread.id,
+        channel: name,
+        text: updateThread.text,
+        time: updateThread.time,
+        sender: updateThread.sender,
+        avatar: updateThread.avatar,
+        creationDate: updateThread.creationDate,
+        timeStamp: updateThread.timeStamp,
+        message: this.updateMessageInThread(updateThread.message, name)
+      }
+      this.firestoreService.updateThread('threads', thread)
+    })
+  }
+
+  public updateMessageInThread(message: Message, name: string): Message {
+    const thread: Message = {
+      id: message.id,
+      channel: name,
+      text: message.text,
+      time: message.time,
+      sender: message.sender,
+      avatar: message.avatar,
+      creationDate: message.creationDate,
+      timeStamp: message.timeStamp
+    }
+    return thread;
   }
 }
